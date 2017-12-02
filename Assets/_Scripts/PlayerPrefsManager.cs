673aecc40a9479cab463da195afbca11ec55eb6a
Jorge Manuel Bello <jorgebell@gmail.com>
2017-12-02 18:47:17 -0400

    added PlayerPrefsManager code

--- Assets/_Scripts/PlayerPrefsManager.cs
+++ Assets/_Scripts/PlayerPrefsManager.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class PlayerPrefsManager : MonoBehaviour {
 
+	public enum Difficulty 
+	{
+		Beginner = 1,
+		Medium,
+		Hard
+	}
+
+	private const string DIFFICULT_LEVEL = "DIFFICULT_LEVEL";
+	private const string MUSIC_VOLUME = "MUSIC_VOLUME";
+	private const string SOUND_VOLUME = "SOUND_VOLUME";
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void setDiffultLevel(Difficulty level)
+	{
+		PlayerPrefs.SetInt (DIFFICULT_LEVEL, (int)level);
+	}
+
+	public int getDifficultLevel()
+	{
+		if (PlayerPrefs.HasKey (DIFFICULT_LEVEL))
+		{
+			return PlayerPrefs.GetInt (DIFFICULT_LEVEL);
+		}
+		return 0;
+	}
+
+	public void setMusicLevel(float level)
+	{
+		PlayerPrefs.SetFloat (MUSIC_VOLUME, level);
+	}
+
+	public float getMusicLevel()
+	{
+		if (PlayerPrefs.HasKey (MUSIC_VOLUME)) 
+		{
+			return PlayerPrefs.GetFloat(MUSIC_VOLUME);
+		}
+		return 0;
+	}
+
+	public void setSoundLevel(float level)
+	{
+		PlayerPrefs.SetFloat (SOUND_VOLUME);
+	}
+
+	public float getSoundLevel()
+	{
+		if (PlayerPrefs.HasKey (SOUND_VOLUME)) 
+		{
+			return PlayerPrefs.GetFloat (SOUND_VOLUME);
+		}
+		return 0;
 	}
 }
